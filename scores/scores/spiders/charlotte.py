998298f704c72a4b8f8e04f5334a3a1f5941a9d7
kcarella <44992176+kcarella@users.noreply.github.com>
2020-09-10 17:04:16 -0700

    Spider can follow link

--- scores/scores/spiders/charlotte.py
+++ scores/scores/spiders/charlotte.py
@@ -11,8 +11,11 @@ class ScoreSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('++++++++Saved file %s' % filename)
+        nextPage = response.xpath('//tbody/tr/td/a[text()="Chael"]/@href').extract()[0]
+        if nextPage is not None:
+            yield response.follow(nextPage, callback=self.afterParse)
+
+    def afterParse(self, response):
+        #self.log("++++++++++++" + "AFTER PARSE!!");
+        with open('example.html', 'wb') as f:
+            f.write(response.body);
